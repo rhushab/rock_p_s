73f58884e181b2120086e809f15dd23455e14942
rhushab <rhushabhbontapallee@googlemail.com>
2022-10-22 06:16:41 +0530

    edit Math.random to exclude 1 in choice

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 function computerChoice() {
-  let choice = Math.floor(Math.random() * 3);
+  let choice = Math.floor(Math.random() * 3 + 1);
   if (choice == 1) return 'rock';
   if (choice == 2) return 'paper';
   if (choice == 3) return 'scissors';
